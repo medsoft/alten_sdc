3af0b7b67a82a5dfd9e0e30136512c3dd1acce22
Mohamed_95 <66523321+medsoft@users.noreply.github.com>
2024-06-10 01:37:12 +0200

    Add files via upload

---
+++ alten.sql
@@ -0,0 +1,66 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le : lun. 10 juin 2024 à 01:32
+-- Version du serveur : 10.4.21-MariaDB
+-- Version de PHP : 7.3.31
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `alten`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `produit`
+--
+
+CREATE TABLE `produit` (
+  `id` bigint(20) NOT NULL,
+  `category` varchar(255) DEFAULT NULL,
+  `code` varchar(255) DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `inventory_status` varchar(255) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `picture` varchar(255) DEFAULT NULL,
+  `price` double NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `rating` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `produit`
+--
+
+INSERT INTO `produit` (`id`, `category`, `code`, `description`, `inventory_status`, `name`, `picture`, `price`, `quantity`, `rating`) VALUES
+(1000, 'Accessories', 'f230fh0g3', 'Product Description', 'INSTOCK', 'Bamboo Watch', NULL, 65, 24, 5),
+(1001, 'Accessories', 'nvklal433', 'Product Description', 'INSTOCK', 'Black Watch', NULL, 72, 61, 4),
+(1002, 'Fitness', 'zz21cz3c1', 'Product Description', 'LOWSTOCK', 'Blue Band', NULL, 79, 2, 3),
+(1003, 'Clothing', '244wgerg2', 'Product Description', 'INSTOCK', 'Blue T-Shirt', NULL, 29, 25, 4);
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `produit`
+--
+ALTER TABLE `produit`
+  ADD PRIMARY KEY (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
